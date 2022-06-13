38b80abc580e299a801694ed718cd43822f3a473
TNPUyen <uyentran14120@gmail.com>
2022-06-13 21:09:22 +0700

    add firebase, material, ngrx, font and icons font

--- apps/olaz-web/src/app/app.module.ts
+++ apps/olaz-web/src/app/app.module.ts
@@ -3,10 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideStorage,getStorage } from '@angular/fire/storage';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule, 
+    BrowserAnimationsModule, 
+    provideFirebaseApp(() => initializeApp(environment.firebase)), 
+    provideAuth(() => getAuth()), 
+    provideDatabase(() => getDatabase()), 
+    provideFirestore(() => getFirestore()), 
+    provideStorage(() => getStorage()), 
+    StoreModule.forRoot({}, {}), 
+    EffectsModule.forRoot([]),
+
+    MatButtonModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- apps/olaz-web/src/environments/environment.prod.ts
+++ apps/olaz-web/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'olaz-90e43',
+    appId: '1:859382530359:web:c3469d145e975f44969ab2',
+    storageBucket: 'olaz-90e43.appspot.com',
+    apiKey: 'AIzaSyBgMSIckJzH7XP4mkQIrC0KDXqgsHE4Ais',
+    authDomain: 'olaz-90e43.firebaseapp.com',
+    messagingSenderId: '859382530359',
+  },
   production: true,
 };
